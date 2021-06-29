0e3bbe9cab27108fa8bb8a570efb7fba6eb71dc9
faweigend <fabian.weigend@westernsydney.edu.au>
2021-06-29 18:51:03 +1000

    add phase A3 to TTE verification

--- tests/tte_phase_tests.py
+++ tests/tte_phase_tests.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 from scipy import optimize
 from threecomphyd.agents.three_comp_hyd_agent import ThreeCompHydAgent
+from threecomphyd.visualiser.three_comp_visualisation import ThreeCompVisualisation
 
 
 def phase_a1():
@@ -22,7 +23,7 @@ def phase_a1():
     return t_end
 
 
-def phase_a2():
+def phase_a2(t1):
     # taken from Equation 11 by Morton 1986
     a = (m_ae * a_ans * (1 - the - gam) + m_ans * (a_anf + a_ans) * (1 - phi)) / (
             a_anf * a_ans * (1 - phi) * (1 - the - gam))
@@ -45,13 +46,18 @@ def phase_a2():
     s_c1 = float(res[0])
     s_c2 = float(res[1])
 
-    def eq_9_substituted(t):
+    # substitute into equation for h
+    def a2_ht(t):
         k1 = r1 * s_c1 * ((a_ans * (1 - the - gam)) / m_ans) + s_c1
         k2 = r2 * s_c2 * ((a_ans * (1 - the - gam)) / m_ans) + s_c2
-        h = k1 * math.exp(r1 * t) + k2 * math.exp(r2 * t) + c / b + the
-        return h - (1 - gam)
+        return k1 * math.exp(r1 * t) + k2 * math.exp(r2 * t) + c / b + the
 
-    t_end = optimize.fsolve(eq_9_substituted, x0=np.array([1]))[0]
+    if phi < gam:
+        # the whole of AnS is utilized in phase A2
+        t_end = optimize.fsolve(lambda t: abs((1 - gam) - a2_ht(t)), x0=np.array([1]))[0]
+    else:
+        # phase A2 transitions into phase A3 before AnS is empty
+        t_end = optimize.fsolve(lambda t: abs((1 - phi) - a2_ht(t)), x0=np.array([1]))[0]
 
     # simulate with the agent for estimated amount of time steps
     agent.reset()
@@ -59,39 +65,82 @@ def phase_a2():
         agent.set_power(p)
         agent.perform_one_step()
 
-    # check if t1 time estimation lines up
-    assert abs(agent.get_h() - (1 - gam)) < 0.001
+    assert abs(agent.get_h() - (1 - max(phi, gam))) < 0.001
 
     # use t2 to get l(t2)
     l_t2 = s_c1 * math.exp(r1 * t_end) + s_c2 * math.exp(r2 * t_end) + c / b
 
-    return l_t2, t_end
+    return t_end, l_t2
 
 
-def phase_a4(l_t2, t_end_t2):
+def phase_a3(t2, lt2):
+    # phase A3 is not applicable if gamma is greater or equal to phi
+    if gam >= phi:
+        return t2, lt2
+
+    a = ((a_anf + a_ans) * m_ans) / (a_anf * a_ans * (1 - the - gam))
+    b = ((p - m_ae) * m_ans) / (a_anf * a_ans * (1 - the - gam))
+
+    def a3_lt(c1, c2, t):
+        return (((b * t) + (c1 * math.exp(-a * t))) / a) + c2
+
+    def a3_dlt(c1, c2, t):
+        return (b / a) - c1 * math.exp(-a * t)
+
+    # derivative l'(t2) can be calculated
+    dlt2 = (m_ans * ((1 - phi) - lt2 - the)) / (a_ans * (1 - the - gam))
+
+    def general_l(p):
+        c1, c2 = p
+        est_lt2 = a3_lt(c1, c2, t2)
+        est_dlt2 = a3_dlt(c1, c2, t2 + 1)
+        # add a small tolerance for derivative estimation
+        com_dl = int(abs(est_dlt2 - dlt2) > 0.001)
+        return abs(est_lt2 - lt2), com_dl
+
+    # solve for c1 and c2
+    res = optimize.fsolve(general_l, x0=np.array([0, 0]))
+    s_c1 = float(res[0])
+    s_c2 = float(res[1])
+
+    def a3_ht(t):
+        return ((a_ans * (1 - the - gam)) / m_ans) * a3_dlt(s_c1, s_c2, t) + a3_lt(s_c1, s_c2, t) + the
+
+    # find end of A3 where h(t3) = (1-gamma)
+    t3 = optimize.fsolve(lambda t: abs((1 - gam) - a3_ht(t)), x0=np.array([t2]))[0]
+
+    # check if t4 time estimation lines up
+    agent.reset()
+    for _ in range(int(t3 * hz)):
+        agent.set_power(p)
+        agent.perform_one_step()
+    assert abs(agent.get_h() - (1 - gam)) < 0.001
+
+    # return t3 and l(t3)
+    return t3, a3_lt(s_c1, s_c2, t3)
+
+
+def phase_a4(t2, lt2):
     # PHASE A4
+    # phase A4 is not applicable if phi is greater or equal to gamma
+    if phi >= gam:
+        return t2, lt2
 
     # FIRST: Determine dynamics of l(t)
     def a4_lt(cx, t):
         return (1 - the - gam) + cx * math.exp((-m_ans * t) / ((1 - the - gam) * a_ans))
 
-    # solve EQ(20)
-    def eq_20(c3):
-        # fit to t2 and known l(t2)
-        l_est_t2 = a4_lt(c3, t_end_t2)
-        return abs(l_t2 - l_est_t2)
+    # find c that fits to known l(t2) = l
+    s_c3 = optimize.fsolve(lambda c: abs(lt2 - a4_lt(c, t2)), x0=np.array([1]))[0]
 
-    # find root
-    s_c3 = optimize.fsolve(eq_20, x0=np.array([1]))[0]
-
-    # check if C3 estimation lines up
-    t_l0 = optimize.fsolve(lambda t: abs((1 - the - gam - 0.1) - a4_lt(s_c3, t)), x0=np.array([t_end_t2]))[0]
+    # double-check if C3 estimation lines up
+    t_l0 = optimize.fsolve(lambda t: abs((1 - the - gam - 0.1) - a4_lt(s_c3, t)), x0=np.array([t2]))[0]
     # simulate with the agent for estimated amount of time steps
     agent.reset()
     for _ in range(int(t_l0 * hz)):
         agent.set_power(p)
         agent.perform_one_step()
-    assert abs(agent.get_g() - (1 - the - gam - 0.1)) < 0.01
+    assert abs(agent.get_g() - (1 - the - gam - 0.1)) < 0.001
 
     # SECOND: Determine dynamics of h(t)
 
@@ -104,26 +153,21 @@ def phase_a4(l_t2, t_end_t2):
     def a4_ht(cx, t):
         return ((-b * math.exp(-k * t)) / (a + k)) + (cx * math.exp(a * t)) - (g / a)
 
-    def eq_21_v3(c4):
-        # we know that at t2 h=(1-gamma)
-        h_act_t2 = (1 - gam)
-        h_est_t2 = a4_ht(c4, t_end_t2)
-        return abs(h_act_t2 - h_est_t2)
-
-    s_c4 = optimize.fsolve(eq_21_v3, x0=np.array([1]))[0]
+    # find c4 that matches h(t2) = (1-gamma)
+    s_c4 = optimize.fsolve(lambda c: abs((1 - gam) - a4_ht(c, t2)), x0=np.array([1]))[0]
 
     # double-check with first derivative and time step t2+1
-    h_act_t2 = (1 - gam)
-    dh_act = (
-            ((-m_ae * (1 - gam)) / ((1 - phi) * a_anf)) -
-            ((m_ans * (1 - the - gam - l_t2)) / ((1 - the - gam) * a_anf)) +
-            (p / a_anf)
-    )
-    h_act_t2p = h_act_t2 + dh_act
-    assert abs(a4_ht(s_c4, t_end_t2 + 1) - h_act_t2p) < 0.001
+    # h_act_t2 = (1 - gam)
+    # dh_act = (
+    #         ((-m_ae * (1 - gam)) / ((1 - phi) * a_anf)) -
+    #         ((m_ans * (1 - the - gam - lt2)) / ((1 - the - gam) * a_anf)) +
+    #         (p / a_anf)
+    # )
+    # h_act_t2p = h_act_t2 + dh_act
+    # assert abs(a4_ht(s_c4, t2 + 1) - h_act_t2p) < 0.0001
 
-    # find time point where h=(1-phi)
-    t_end = optimize.fsolve(lambda t: abs((1 - phi) - a4_ht(s_c4, t)), x0=np.array([t_end_t2]))[0]
+    # find end of phase A4. The time point where h(t4)=(1-phi)
+    t_end = optimize.fsolve(lambda t: abs((1 - phi) - a4_ht(s_c4, t)), x0=np.array([t2]))[0]
 
     # check if t4 time estimation lines up
     agent.reset()
@@ -132,7 +176,40 @@ def phase_a4(l_t2, t_end_t2):
         agent.perform_one_step()
     assert abs(agent.get_h() - (1 - phi)) < 0.001
 
+    lt_end = a4_lt(s_c3, t_end)
     # finally return t4 result
+    return t_end, lt_end
+
+
+def phase_a5(t4, lt4):
+    # FIRST: Determine dynamics of l(t)
+    def a5_lt(cx, t):
+        return (1 - the - gam) + cx * math.exp((-m_ans * t) / ((1 - the - gam) * a_ans))
+
+    # find c that fits to known l(t4) = given l
+    s_cl = optimize.fsolve(lambda c: abs(lt4 - a5_lt(c, t4)), x0=np.array([1]))[0]
+
+    # SECOND: determine dynamics of h(t)
+    k = m_ans / ((1 - the - gam) * a_ans)
+    a = (p - m_ae) / a_anf
+    b = (m_ans * s_cl) / ((1 - the - gam) * a_anf)
+
+    def a5_ht(cx, t):
+        return (a * t) - ((b * math.exp(-k * t)) / k) + cx
+
+    # find c5 that matches h(t4) = (1-phi)
+    s_ch = optimize.fsolve(lambda c: abs((1 - phi) - a5_ht(c, t4)), x0=np.array([1]))[0]
+
+    # find end of phase A5. The time point where h(t5)=1
+    t_end = optimize.fsolve(lambda t: abs(1 - a5_ht(s_ch, t)) + int(t < t4), x0=np.array([t4]))[0]
+
+    # check if t5 time estimation lines up
+    agent.reset()
+    for _ in range(int(t_end * hz)):
+        agent.set_power(p)
+        agent.perform_one_step()
+    assert abs(agent.get_h() - 1) < 0.001
+
     return t_end
 
 
@@ -141,16 +218,16 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s %(levelname)-5s %(name)s - %(message)s. [file=%(filename)s:%(lineno)d]")
 
-    hz = 40
+    hz = 50
 
     example_conf = [11532.526538727172,
-                    543240.257042239595,
+                    424324.257042239595,
                     249.7641585019016,
                     286.26673813946095,
                     7.988078323028352,
                     0.25486842730772163,
-                    0.26874299216869681,
-                    0.2141766056862277]
+                    0.2141766056862277,
+                    0.2641766056862277]
 
     a_anf = example_conf[0]
     a_ans = example_conf[1]
@@ -165,9 +242,15 @@ if __name__ == "__main__":
     agent = ThreeCompHydAgent(hz=hz, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae, m_ans=m_ans,
                               m_anf=m_anf, the=the, gam=gam, phi=phi)
 
+    # ThreeCompVisualisation(agent)
+
     p = 400
 
     # all TTE phases
     t1 = phase_a1()
-    lt2, t2 = phase_a2()
-    t4 = phase_a4(lt2, t2)
+    t2, lt2 = phase_a2(t1)
+    t3, lt3 = phase_a3(t2, lt2)
+    t4, lt4 = phase_a4(t3, lt3)
+    t5 = phase_a5(t4, lt4)
+
+    print(t5)
