06cd68a0fb321791926a8489bce12c9d72ccc073
faweigend <fabian.weigend@westernsydney.edu.au>
2021-06-30 11:31:56 +1000

    move data storage directory configuration into example scripts for clarification

--- example_scripts/grid_search_evolutionary_parameters.py
+++ example_scripts/grid_search_evolutionary_parameters.py
@@ -1,17 +1,24 @@
 import logging
+import os
 
 from threecomphyd.evolutionary_fitter.pygmo_three_comp_fitter import PyGMOThreeCompFitter
-from threecomphyd.evolutionary_fitter.three_comp_tools import prepare_caen_recovery_ratios, prepare_standard_tte_measures
+from threecomphyd.evolutionary_fitter.three_comp_tools import prepare_caen_recovery_ratios, \
+    prepare_standard_tte_measures
+
+import threecomphyd.config as config
 
 if __name__ == "__main__":
     # set logging level to highest level
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s %(levelname)-5s %(name)s - %(message)s. [file=%(filename)s:%(lineno)d]")
 
+    # set the path to store the results into
+    config.paths["data_storage"] = os.path.dirname(os.path.abspath(__file__)) + "/../data-storage/"
+
     # group averages from Caen et al. 2019
     caen = (
-        18200, # W'
-        248 # CP
+        18200,  # W'
+        248  # CP
     )
 
     # define wp cp combination in use
@@ -27,6 +34,9 @@ if __name__ == "__main__":
     fitter = PyGMOThreeCompFitter(ttes=ttes, recovery_measures=recovery_measures)
 
     # Grid search starts here
+    # islands run the evolutionary algorithm in parallel and exchange solutions. One island = one thread
     islands = [7, 14, 21]
     for isl in islands:
+        # run grid search with given amount of islands and save results to
+        # config.paths["data_storage"] + "THREE_COMP_PYGMO_FIT" folder
         fitter.grid_search_algorithm_moead(islands=isl)
--- example_scripts/pygmo_fittings_report.py
+++ example_scripts/pygmo_fittings_report.py
@@ -1,16 +1,24 @@
 import logging
+import os
+
 from threecomphyd.handler.pygmo_fitting_report_creator import PyGMOFittingReportCreator
+
+import threecomphyd.config as config
 
 if __name__ == "__main__":
     # set logging level to highest level
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s %(levelname)-5s %(name)s - %(message)s. [file=%(filename)s:%(lineno)d]")
 
-    # Create report creator object and run report creation
+    # set the path to read the results from. The ReportCreator will look for a THREE_COMP_PYGMO_FIT directory
+    config.paths["data_storage"] = os.path.dirname(os.path.abspath(__file__)) + "/../data-storage/"
+
+    # Create report creator object
     src = PyGMOFittingReportCreator()
 
-    # saves fitting results in a readable json format to the data-storage folder
+    # run report creation, which saves fitting results in a readable json format into the
+    # config.paths["data_storage"] + "PYGMO_FITTING_REPORT_CREATOR" folder
     src.write_data_report(clear_all=True)
 
-    # creates the grid search overview table for the Appendix
+    # creates the grid search overview table for the Appendix of Weigend et al. 2021
     # src.create_latex_table()
