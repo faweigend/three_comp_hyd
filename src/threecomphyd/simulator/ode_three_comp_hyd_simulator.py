aecf8f3db2b1cf14d482bbd11c91051ce2194f61
faweigend <fabian.weigend@westernsydney.edu.au>
2021-06-30 20:57:13 +1000

    improve test procedure to highlight were fittings go wrong. Add handling of equilibria

--- src/threecomphyd/simulator/ode_three_comp_hyd_simulator.py
+++ src/threecomphyd/simulator/ode_three_comp_hyd_simulator.py
@@ -1,3 +1,4 @@
+import logging
 import math
 
 import numpy as np
@@ -9,27 +10,41 @@ class ODEThreeCompHydSimulator:
     Simulates Three Component Hydraulic Model responses using Ordinary Differential Equations
     """
 
+    tolerance = 8
+    max_steps = 5000
+
     @staticmethod
-    def phase_a1(p: float, a_anf: float, m_ae: float, theta: float, phi: float) -> float:
+    def phase_a1(p: float, a_anf: float, m_ae: float, theta: float, phi: float) -> (float, float, float):
+
+        # check if equilibrium in phase A1
+        if ((m_ae * min(theta, 1 - phi)) / (p * (1 - phi))) >= 1:
+            h = (a_anf * (1 - phi)) / m_ae * (1 - np.exp(-(m_ae * np.inf) / (a_anf * (1 - phi)))) * p / a_anf
+            return np.inf, h, 0
+
         # end of phase A1 -> the time when h(t) = min(theta,1-phi)
         t1 = (-(a_anf * (1 - phi)) / m_ae) * math.log(1 - ((m_ae * min(theta, 1 - phi)) / (p * (1 - phi))))
-        return t1
+
+        # return t1, h(t1), g(t1)
+        return t1, min(theta, 1 - phi), 0
 
     @staticmethod
-    def phase_a2(t1: float, p: float, a_anf: float, m_ae: float, theta: float, phi: float) -> float:
+    def phase_a2(t1: float, ht1: float, gt1: float, p: float, a_anf: float, m_ae: float, theta: float, phi: float) -> (
+            float, float, float):
         # A2 is only applicable if phi is higher than the top of AnS
         if phi <= (1 - theta):
-            return t1
-        # linear utilization
-        return t1 + ((phi - (1 - theta)) * a_anf) / (p - m_ae)
+            return t1, ht1, gt1
+        # linear utilization -> no equilibrium possible
+        t2 = t1 + ((phi - (1 - theta)) * a_anf) / (p - m_ae)
+        # return t2, h(t2), g(t2)
+        return t2, theta, gt1
 
     @staticmethod
-    def phase_a3(t2: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float, theta: float,
-                 gamma: float, phi: float) -> (float, float):
+    def phase_a3(t2: float, ht2: float, gt2: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
+                 theta: float, gamma: float, phi: float) -> (float, float, float):
 
         # A3 is only applicable if flow from Ae is not at max
         if phi > (1 - theta):
-            return t2, 0
+            return t2, ht2, gt2
 
         # taken from Equation 11 by Morton 1986
         a = (m_ae * a_ans * (1 - theta - gamma) + m_ans * (a_anf + a_ans) * (1 - phi)) / (
@@ -45,37 +60,42 @@ class ODEThreeCompHydSimulator:
 
         def a3_gt(c1, c2, t):
             # the general solution for g(t)
-            return c1 * math.exp(r1 * t) + c2 * math.exp(r2 * t) + c / b
+            return c1 * np.exp(r1 * t) + c2 * math.exp(r2 * t) + c / b
 
         def a3_dgt(c1, c2, t):
             # derivative of general solution
-            return r1 * c1 * math.exp(r1 * t) + r2 * c2 * math.exp(r2 * t)
+            return r1 * c1 * np.exp(r1 * t) + r2 * c2 * math.exp(r2 * t)
 
         # Find c1 and c2 that satisfy g(t2) and dg(t2) = 0
-        res = optimize.fsolve(lambda cx: (a3_gt(cx[0], cx[1], t2), a3_dgt(cx[0], cx[1], t2)), x0=np.array([1, 1]))
+        res = optimize.fsolve(lambda cx: [
+            a3_gt(cx[0], cx[1], t2),
+            a3_dgt(cx[0], cx[1], t2)
+        ], x0=np.array([1, 1]))
+
         s_c1 = float(res[0])
         s_c2 = float(res[1])
 
         # substitute into EQ(9) for h
-        def a2_ht(t):
+        def a3_ht(t):
             return (a_ans * (1 - theta - gamma)) / m_ans * a3_dgt(s_c1, s_c2, t) + a3_gt(s_c1, s_c2, t) + theta
 
-        if phi < gamma:
-            # the whole of AnS is utilized in phase A3
-            t3 = optimize.fsolve(lambda t: abs((1 - gamma) - a2_ht(t)), x0=np.array([1]))[0]
-        else:
-            # phase A3 transitions into phase A4 before AnS is empty
-            t3 = optimize.fsolve(lambda t: abs((1 - phi) - a2_ht(t)), x0=np.array([1]))[0]
+        # if phi > gamma, then phase A3 transitions into phase A4 before AnS is empty
+        ht3 = 1 - max(phi, gamma)
 
-        # use t3 to get g(t3)
-        return t3, a3_gt(s_c1, s_c2, t3)
+        # check if equilibrium in this phase
+        if ht2 <= a3_ht(np.inf) <= ht3:
+            return np.inf, a3_ht(np.inf), a3_gt(s_c1, s_c2, np.inf)
+        else:
+            t3 = optimize.fsolve(lambda t: abs(ht3 - a3_ht(t)), x0=np.array([t2]))[0]
+            # use t3 to get g(t3)
+            return t3, ht3, a3_gt(s_c1, s_c2, t3)
 
     @staticmethod
-    def phase_a4(t3: float, gt3: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
+    def phase_a4(t3: float, ht3: float, gt3: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
                  theta: float, gamma: float, phi: float) -> (float, float):
         # phase A3 is not applicable if gamma is greater or equal to phi
         if gamma >= phi:
-            return t3, gt3
+            return t3, ht3, gt3
 
         a = ((a_anf + a_ans) * m_ans) / (a_anf * a_ans * (1 - theta - gamma))
         b = ((p - m_ae) * m_ans) / (a_anf * a_ans * (1 - theta - gamma))
@@ -84,12 +104,11 @@ class ODEThreeCompHydSimulator:
             # general solution for g(t)
             return (((b * t) + (c1 * math.exp(-a * t))) / a) + c2
 
-        def a4_dgt(c1, c2, t):
+        def a4_dgt(c1, _, t):
             # first derivative g'(t)
             return (b / a) - c1 * math.exp(-a * t)
 
         # derivative g'(t3) can be calculated manually
-        ht3 = max(theta, 1 - phi)
         dgt3 = (m_ans * (ht3 - gt3 - theta)) / (a_ans * (1 - theta - gamma))
 
         def general_gt(p):
@@ -109,47 +128,54 @@ class ODEThreeCompHydSimulator:
             # EQ(9) with constants for g(t) and g'(t)
             return ((a_ans * (1 - theta - gamma)) / m_ans) * a4_dgt(s_c1, s_c2, t) + a4_gt(s_c1, s_c2, t) + theta
 
-        # find end of A4 where g(t4) = (1-gamma)
-        t4 = optimize.fsolve(lambda t: abs((1 - gamma) - a4_ht(t)), x0=np.array([t3]))[0]
-
-        # return t4 and g(t4)
-        return t4, a4_gt(s_c1, s_c2, t4)
+        ht4 = (1 - gamma)
+        # check if equilibrium in this phase
+        if ht3 <= a4_ht(np.inf) <= ht4:
+            return np.inf, a4_ht(np.inf), a4_gt(s_c1, s_c2, np.inf)
+        else:
+            # find end of A4 where g(t4) = (1-gamma)
+            t4 = optimize.fsolve(lambda t: abs(ht4 - a4_ht(t)), x0=np.array([t3]))[0]
+            return t4, ht4, a4_gt(s_c1, s_c2, t4)
 
     @staticmethod
-    def phase_a5(t4: float, gt4: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
+    def phase_a5(t4: float, ht4: float, gt4: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
                  theta: float, gamma: float, phi: float) -> (float, float):
 
         # phase A5 is not applicable if phi is greater or equal to gamma
         if phi >= gamma:
-            return t4, gt4
+            return t4, ht4, gt4
 
         def a5_gt(c, t):
             # generalised g(t) for phase A5
             return (1 - theta - gamma) + c * math.exp((-m_ans * t) / ((1 - theta - gamma) * a_ans))
 
         # find c that fits to known g(t4) = gt4
-        s_c1 = optimize.fsolve(lambda c: abs(gt4 - a5_gt(c, t4)), x0=np.array([1]))[0]
+        s_cg = optimize.fsolve(lambda c: abs(gt4 - a5_gt(c, t4)), x0=np.array([1]))[0]
 
         # as defined for EQ(21)
         k = m_ans / ((1 - theta - gamma) * a_ans)
         a = -m_ae / ((1 - phi) * a_anf)
-        b = (m_ans * s_c1) / ((1 - theta - gamma) * a_anf)
+        b = (m_ans * s_cg) / ((1 - theta - gamma) * a_anf)
         g = p / a_anf
 
         def a5_ht(c, t):
             return ((-b * math.exp(-k * t)) / (a + k)) + (c * math.exp(a * t)) - (g / a)
 
-        # find c that matches g(t4) = (1-gamma)
-        s_c2 = optimize.fsolve(lambda c: abs((1 - gamma) - a5_ht(c, t4)), x0=np.array([1]))[0]
+        # find c that matches h(t4) = ht4
+        s_ch = optimize.fsolve(lambda c: abs(ht4 - a5_ht(c, t4)), x0=np.array([1]))[0]
 
-        # solve for time point where phase A5 ends h(t5)=(1-phi)
-        t5 = optimize.fsolve(lambda t: abs((1 - phi) - a5_ht(s_c2, t)), x0=np.array([t4]))[0]
-
-        # return t5 and g(t5)
-        return t5, a5_gt(s_c1, t5)
+        ht5 = (1 - phi)
+        # check if equilibrium in this phase
+        if ht4 <= a5_ht(np.inf, s_ch) <= ht5:
+            return np.inf, a5_ht(np.inf, s_ch), a5_gt(s_cg, np.inf)
+        else:
+            # solve for time point where phase A5 ends h(t5)=(1-phi)
+            t5 = optimize.fsolve(lambda t: abs(ht5 - a5_ht(s_ch, t)), x0=np.array([t4]))[0]
+            return t5, ht5, a5_gt(s_cg, t5)
 
     @staticmethod
-    def phase_a6(t5: float, gt5: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float, theta: float,
+    def phase_a6(t5: float, ht5: float, gt5: float, p: float, a_anf: float, a_ans: float, m_ae: float, m_ans: float,
+                 theta: float,
                  gamma: float, phi: float) -> (float, float):
 
         def a6_gt(c, t):
@@ -163,16 +189,15 @@ class ODEThreeCompHydSimulator:
         a = (p - m_ae) / a_anf
         b = (m_ans * s_cg) / ((1 - theta - gamma) * a_anf)
 
-        def a6_ht(cx, t):
+        def a6_ht(cx, t) -> float:
             # generalised h(t) for phase A6
             return (a * t) - ((b * math.exp(-k * t)) / k) + cx
 
-        # find ch that matches h(t5) = (1-phi)
-        s_ch = optimize.fsolve(lambda c: abs((1 - phi) - a6_ht(c, t5)), x0=np.array([1]))[0]
+        # find ch that matches h(t5) = ht5
+        s_ch = optimize.fsolve(lambda c: np.abs(ht5 - a6_ht(c, t5)), x0=np.array([1]))[0]
 
+        ht6 = 1
         # find end of phase A6. The time point where h(t6)=1
         # condition t > t5 added
-        t6 = optimize.fsolve(lambda t: abs(1 - a6_ht(s_ch, t)) + int(t < t5), x0=np.array([t5]))[0]
-
-        # return time to exhaustion (t6) and g(t6)
-        return t6, a6_gt(s_cg, t6)
+        t6 = optimize.fsolve(lambda t: np.abs(ht6 - a6_ht(s_ch, t)) + int(t < t5), x0=np.array([t5]))[0]
+        return t6, ht6, a6_gt(s_cg, t6)
--- tests/tte_phase_tests.py
+++ tests/tte_phase_tests.py
@@ -1,15 +1,17 @@
-import logging
-import warnings
-
-warnings.filterwarnings("error")
-
 from threecomphyd.agents.three_comp_hyd_agent import ThreeCompHydAgent
 from threecomphyd.evolutionary_fitter.three_comp_tools import MultiObjectiveThreeCompUDP
 from threecomphyd.visualiser.three_comp_visualisation import ThreeCompVisualisation
 from threecomphyd.simulator.ode_three_comp_hyd_simulator import ODEThreeCompHydSimulator
 
+import logging
+import warnings
 
-def test_procedure(hz, eps, conf):
+import numpy as np
+
+warnings.filterwarnings("error")
+
+
+def test_procedure(hz, eps, conf, agent):
     a_anf = conf[0]
     a_ans = conf[1]
     m_ae = conf[2]
@@ -19,49 +21,46 @@ def test_procedure(hz, eps, conf):
     gam = conf[6]
     phi = conf[7]
 
-    # create three component hydraulic agent with example configuration
-    agent = ThreeCompHydAgent(hz=hz, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae, m_ans=m_ans,
-                              m_anf=m_anf, the=the, gam=gam, phi=phi)
-
-    # ThreeCompVisualisation(agent)
-
-    p = 300
+    p = 350
 
     # all TTE phases
-    try:
-        t1 = ODEThreeCompHydSimulator.phase_a1(p=p, a_anf=a_anf, m_ae=m_ae, theta=the, phi=phi)
-        t2 = ODEThreeCompHydSimulator.phase_a2(t1=t1, p=p, a_anf=a_anf, m_ae=m_ae, theta=the, phi=phi)
-    except ValueError:
-        logging.info("A1/2 error with {}".format(example_conf))
+    t1, ht1, gt1 = ODEThreeCompHydSimulator.phase_a1(p=p, a_anf=a_anf, m_ae=m_ae, theta=the, phi=phi)
+    if t1 == np.inf:
+        logging.info("EQUILIBRIUM IN A1: t: {} h: {} g: {}".format(t1, ht1, gt1))
         return
+    logging.info("A1: {}".format(t1))
 
-    try:
-        t3, gt3 = ODEThreeCompHydSimulator.phase_a3(t2=t2, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
-                                                    m_ans=m_ans, theta=the, gamma=gam, phi=phi)
-    except RuntimeWarning:
-        logging.info("A3 warning with {}".format(example_conf))
-        return
+    t2, ht2, gt2 = ODEThreeCompHydSimulator.phase_a2(t1=t1, ht1=ht1, gt1=gt1, p=p, a_anf=a_anf, m_ae=m_ae, theta=the,
+                                                     phi=phi)
+    logging.info("A2: {}".format(t2))
 
-    try:
-        t4, gt4 = ODEThreeCompHydSimulator.phase_a4(t3=t3, gt3=gt3, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
-                                                    m_ans=m_ans, theta=the, gamma=gam, phi=phi)
-    except RuntimeWarning:
-        logging.info("A4 warning with {}".format(example_conf))
+    t3, ht3, gt3 = ODEThreeCompHydSimulator.phase_a3(t2=t2, ht2=ht2, gt2=gt2, p=p, a_anf=a_anf, a_ans=a_ans,
+                                                     m_ae=m_ae, m_ans=m_ans, theta=the, gamma=gam, phi=phi)
+    if t3 == np.inf:
+        logging.info("EQUILIBRIUM IN A3: t: {} h: {} g: {}".format(t3, ht3, gt3))
         return
+    logging.info("A3: {}".format(t3))
 
-    try:
-        t5, gt5 = ODEThreeCompHydSimulator.phase_a5(t4=t4, gt4=gt4, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
-                                                    m_ans=m_ans, theta=the, gamma=gam, phi=phi)
-    except RuntimeWarning:
-        logging.info("A5 warning with {}".format(example_conf))
+    t4, ht4, gt4 = ODEThreeCompHydSimulator.phase_a4(t3=t3, ht3=ht3, gt3=gt3, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
+                                                     m_ans=m_ans, theta=the, gamma=gam, phi=phi)
+    if t4 == np.inf:
+        logging.info("EQUILIBRIUM IN A4: t: {} h: {} g: {}".format(t4, ht4, gt4))
         return
+    logging.info("A4: {}".format(t4))
 
-    try:
-        t6, gt6 = ODEThreeCompHydSimulator.phase_a6(t5=t5, gt5=gt5, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
-                                                    m_ans=m_ans, theta=the, gamma=gam, phi=phi)
-    except RuntimeWarning:
-        logging.info("A6 warning with {}".format(example_conf))
+    t5, ht5, gt5 = ODEThreeCompHydSimulator.phase_a5(t4=t4, ht4=ht4, gt4=gt4, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
+                                                     m_ans=m_ans, theta=the, gamma=gam, phi=phi)
+    if t5 == np.inf:
+        logging.info("EQUILIBRIUM IN A5: t: {} h: {} g: {}".format(t5, ht5, gt5))
         return
+    logging.info("A5: {}".format(t5))
+
+    t6, ht6, gt6 = ODEThreeCompHydSimulator.phase_a6(t5=t5, ht5=ht5, gt5=gt5, p=p, a_anf=a_anf, a_ans=a_ans, m_ae=m_ae,
+                                                     m_ans=m_ans, theta=the, gamma=gam, phi=phi)
+    if t6 == np.inf:
+        logging.info("EQUILIBRIUM IN A6: t: {} h: {} g: {}".format(t6, ht6, gt6))
+        return
+    logging.info("A6: {}".format(t6))
 
     # A1
     agent.reset()
@@ -118,4 +117,11 @@ if __name__ == "__main__":
 
     example_conf = udp.create_educated_initial_guess()
 
-    test_procedure(hz, eps, example_conf)
+    # create three component hydraulic agent with example configuration
+    agent = ThreeCompHydAgent(hz=hz, a_anf=example_conf[0], a_ans=example_conf[1], m_ae=example_conf[2],
+                              m_ans=example_conf[3], m_anf=example_conf[4], the=example_conf[5],
+                              gam=example_conf[6], phi=example_conf[7])
+
+    ThreeCompVisualisation(agent)
+
+    test_procedure(hz, eps, example_conf, agent)
